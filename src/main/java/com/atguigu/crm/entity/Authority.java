7767671df0957dbfccb25537528a6544f6e26064
yan <yp_jobs@163.com>
2019-04-09 22:29:19 +0800

    testgit

--- src/main/java/com/atguigu/crm/entity/Authority.java
+++ src/main/java/com/atguigu/crm/entity/Authority.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Authority extends BaseIdEntity implements Serializable {
-
+    //随便修改
     private static final long serialVersionUID = 1L;
     /**
      * 权限的名字, 用于显示。
---
+++ src/main/java/com/atguigu/crm/entity/test11111.java
@@ -0,0 +1,15 @@
+package com.atguigu.crm.entity;
+
+/**
+ * TODO
+ *
+ * @ClassName: test11111
+ * @author: yan
+ * @since: 2019/4/9 22:22
+ */
+public class test11111 {
+    public static  void main(String[] args) {
+        String str = "";
+        System.out.println("============");
+    }
+}
